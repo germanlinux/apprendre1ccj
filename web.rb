bfab31ebb09c866ad463ca6f7353fe72c062132a
Eric German <german.eric@gmail.com>
2012-05-22 18:41:59 +0200

    add sentence

--- web.rb
+++ web.rb
@@ -6,12 +6,18 @@ require 'sinatra'
 mode_debug = "Mode debug OFF<p>"
 configure do
     data = ''
-        f  = File.open('Gemfile.lock','r') 
-            f.each_line do |line| 
-            data << "#{line}" 
-            end
-     set :infogem,data 
-
+    f  = File.open('Gemfile.lock','r') 
+      f.each_line do |line| 
+      data << "#{line}" 
+    end
+    
+    set :infogem,data 
+    sentences = []
+      f  = File.open('segments2.csv','r') 
+      f.each_line do |line| 
+      sentences.push line 
+    end
+    set :phrases, sentences
 end
 before do
     # debug available in  local but not in cloud
@@ -19,6 +25,11 @@ before do
         puts "#{request.request_method} #{request.path_info}"
         mode_debug = "Mode debug ON<p>"
     end
+    time = Time.new
+    @quantieme  = time.yday
+    @madate    = time.strftime("%d/%m/%Y")
+    @quantieme  = time.yday - 139
+    
 end
 get '/about' do
     @mode_debug = mode_debug
@@ -27,5 +38,29 @@ get '/about' do
 end
 
 get '/' do
+   une_phrase = settings.phrases[@quantieme]  
+   un_jour = une_phrase.split(/;/)
+   @tag     =  un_jour[1]
+   if @tag  == "Industrie" then 
+        @ltag =  '<span class="btn-info btn-large disabled">' + @tag + '</span>'
+   end
+   if @tag  == "Informatique" then 
+        @ltag =  '<span class="btn-success btn-large disabled">' + @tag + '</span>'
+   end
+if @tag  == "Histoire" then 
+        @ltag =  '<span class="btn-danger btn-large disabled">' + @tag + '</span>'
+   end
+if @tag  == "Geo" then 
+        @ltag =  '<span class="btn-warning btn-large disabled">' + @tag + '</span>'
+   end
+if @tag  == "Sport" then 
+        @ltag =  '<span class="btn-inverse btn-large disabled">' + @tag + '</span>'
+   end
+
+   @content =  un_jour[2]
+   un_jour[5].chomp!
+   if un_jour[5].to_i > 0 then 
+    @content += "<p>#{un_jour[4]}<p>"
+   end
    erb :index
 end
